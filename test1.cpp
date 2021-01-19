0629be73f95456992afadf93cd754baed8a22410
zhang-jingwen <77620764+zhang-jingwen@users.noreply.github.com>
2021-01-19 20:07:19 +0800

    Update test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -3,7 +3,15 @@ using namespace std;
 
 int main()
 {
-    printf("Hello World\n");
+    printf("Hello World£¬and Hello sky\n");
     printf("jingwen");
+    printf("1122334455");
+    int a, b, c;
+    a = 12;
+    b = 13;
+    c = 4;
+    int d;
+    d = a + b + c;
+
     return 0;
 }
